cda634b3cb32f1ee44cb2fa56ddccc6ed2086561
Chenguang Wang <464039958@qq.com>
2023-03-22 14:17:25 +0800

    链表的中间结点

--- algorithm/链表的中间结点.go
+++ algorithm/链表的中间结点.go
@@ -5,8 +5,15 @@ package algorithm
 // 如果有两个中间结点，则返回第二个中间结点。
 
 // 双指针思路，使用快慢指针
-// 链表长度分奇偶两种情况讨论
+// 链表长度分奇偶两种情况考虑
 func middleNode(head *ListNode) *ListNode {
-
-	return nil
+	low, fast := head, head
+	for fast.Next != nil {
+		low = low.Next
+		fast = fast.Next.Next
+	}
+	if fast.Next != nil {
+		low = low.Next
+	}
+	return low
 }
